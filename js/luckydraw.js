579a92e26f2a971051f31e3aa563f512fca396c1
Hong Nguyen <nghong@nghong-mac.local>
2020-01-08 09:08:35 +0700

    Initialization

--- js/luckydraw.js
+++ js/luckydraw.js
@@ -2,6 +2,11 @@ function drawNext() {
   var lastdraw = $("#luckyperson").html();
   var alllucky = $("#drawhistory").html();
   $("#drawhistory").html(lastdraw + '<br />' + alllucky);
-  var personname = data[Math.floor(Math.random() * data.length)];
-  $("#luckyperson").html(personname);
+  var randomnumber = Math.floor(Math.random() * data.length);
+  var luckynumber = data[randomnumber];
+  $("#luckyperson").html(luckynumber);
+  data.splice(randomnumber,1);
+  if (data.length == 0) {
+    $("#btnDraw").prop('disabled', true);
+  }
 }
